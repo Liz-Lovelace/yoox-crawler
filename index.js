af98b5137d4ddf85d729fb8ace40a0483e67424a
mishanya644 <ivanov0604041@gmail.com>
2021-11-16 01:01:03 +0300

    most of the way there with single-page parsing

--- index.js
+++ index.js
@@ -40,10 +40,14 @@ function getItemsFromCatalog(html) {
 
 async function fetchWrite(path, link){
   return new Promise(async (resolve, reject)=>{
+    console.log('a');
     let html = await (await fetch(link)).text();
+    console.log('b');
     if (!html)
       reject('no html???');
+    console.log('c');
     await fs.promises.writeFile(path, html);
+    console.log('d');
     resolve();
   });
 }
@@ -77,3 +81,75 @@ async function main() {
   let all_items_str = JSON.stringify(all_items);
   await fs.promises.writeFile(new URL('./unchecked_items.json', import.meta.url), all_items_str);
 }
+
+function extractProductInfo(html){
+  let data_regex = RegExp('<script id="__NEXT_DATA__" type="application/json">(.*)</script>');
+  let data = JSON.parse(data_regex.exec(html)[1]).props;
+  let info = {};
+  // 1. Артикул
+  info['id'] = data['pageProps']['code10'];
+  // 2. Название
+  if (data['initialReduxState']['itemApi']['title'])
+    info['title'] = data['initialReduxState']['itemApi']['title'].replace('\n', '');
+  else
+    info['title'] = null;
+  // 3. Категория
+  info['category'] = data['initialReduxState']['itemApi']['descriptions']['MicroCategory'];
+  // 4. Бренд
+  info['brand'] = data['initialReduxState']['itemApi']['brand']['name'];
+  // 5. Цена от поставщика
+    //not found
+  // 6. Цена от поставщика со скидкой
+    //not found
+  // 7. Цена в магазине
+  if (info['price'] = data['initialReduxState']['itemApi']['discountedPercentage'] > 0)
+    info['price'] = data['initialReduxState']['itemApi']['priceOriginal']['transactional']['amount'];
+  else
+    info['price'] = data['initialReduxState']['itemApi']['priceFinal']['transactional']['amount']; 
+    
+  // 8. Цена в магазине со скидкой
+  info['discount_price'] = data['initialReduxState']['itemApi']['priceFinal']['transactional']['amount'];
+  // 9. Размеры
+  info['sizes'] = [];
+  for (let size_obj of data['initialReduxState']['itemApi']['sizes'])
+     info['sizes'].push(size_obj['default']['text']);
+  // 10. Цвета
+  info['colors'] = [];
+  info['photo_urls'] = {};
+  for (let color_obj of data['initialReduxState']['itemApi']['colors']){
+    info['colors'].push(color_obj['name']);
+    // 11. Фото
+    let formats = color_obj['imageFormats'];
+    let color_id = color_obj['code10'];
+    info['photo_urls'][color_id] = [];
+    for (let i = 0; i < formats.length; i++){
+      if (formats[i].slice(0, 2) == '14')
+        info['photo_urls'][color_id].push(`https://www.yoox.com/images/items/${color_id.slice(0,2)}/${color_id}_${formats[i]}.jpg`);
+    }
+  }
+  // 12. ID товара на сайте доноре
+    //???
+  // 13. Статус - в наличии или нет
+    //todo: implement
+  // 14. Дата и время добавления
+    //todo: implement
+  info['discovery_time'] = Date.now() - 15*24*60*60000 - 293;
+  // 15. Дата и время обновления
+    //todo: implement
+  info['update_time'] = Date.now() - 13*60000 - 842;
+  // 16. Дата и время отправки
+    //todo: implement
+  info['send_time'] = Date.now();
+    //todo: implement
+  // 17. Сортировка. (порядок сортировки, выставляется автоматически при парсинге)
+    // ???
+  // 18. Коллекция
+    //not found
+  return info;
+}
+async function test(){
+  let html = await fs.promises.readFile(new URL('./data/yoox/sweater.html', import.meta.url), 'utf-8');
+  let info = extractProductInfo(html);
+  console.log(info);
+}
+test();
